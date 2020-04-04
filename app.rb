47a05ee816f965f9f37fc01caa365bc725219fc5
Angelica <Angelica137@users.noreply.github.com>
2020-04-04 19:09:04 +0100

    add cat pic

--- app.rb
+++ app.rb
@@ -7,3 +7,13 @@ end
 get '/secret' do
   'Here I am'
 end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
+
+get '/shopping list' do
+  'add what else you need'
+end
